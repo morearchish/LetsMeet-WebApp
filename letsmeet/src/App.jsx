08f6ca63eea1bf9712c7426f8969669fe39f6c8b
Archish <archish28022001@gmail.com>
2023-10-19 00:01:46 +0530

    front-end changes

--- letsmeet/src/App.jsx
+++ letsmeet/src/App.jsx
@@ -13,16 +13,20 @@ function App() {
 
           <div style={{ gap: "50px", display: "grid" }}>
             <div>
-              <p style={{ fontWeight: "bold", fontSize: "20px", marginTop: "0px", marginBottom: "0px" }}>User Name</p>
-              <textarea style={{ width: "100%" }}></textarea>
-
+              <form id="login-form">
+                <p style={{ fontWeight: "bold", fontSize: "20px", marginTop: "0px", marginBottom: "0px" }}>User Name</p>
+                <input style={{ height: "35px", width: "480px" }} type="text" name="username" id="username" />
+              </form>
             </div>
             <div>
-              <p style={{ fontWeight: "bold", fontSize: "20px", marginTop: "0px", marginBottom: "0px" }}>Password</p>
-              <textarea style={{ width: "100%" }}></textarea>
-
+              <form id="login-form">
+                <p style={{ fontWeight: "bold", fontSize: "20px", marginTop: "0px", marginBottom: "0px" }}>Password</p>
+                <input style={{ width: "480px", height: "35px" }} type="password" name="password" id="password" />
+              </form>
             </div>
-            <button>LOGIN IN</button>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <button style={{ height: "50px", width: "300px" }} id="login-button">LOGIN IN</button>
+            </div>
 
           </div>
 
